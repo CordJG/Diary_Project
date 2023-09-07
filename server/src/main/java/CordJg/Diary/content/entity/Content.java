5c3fdc6be384426115f3a2a4332d08b428ddb020
CordJG <wjrmffldglem@gmail.com>
2023-09-07 19:26:48 +0900

    content column modify

--- server/src/main/java/CordJg/Diary/content/entity/Content.java
+++ server/src/main/java/CordJg/Diary/content/entity/Content.java
@@ -26,7 +26,7 @@ public class Content  {
     @Column(nullable = false)
     private String title;
 
-    @Column(nullable = false)
+    @Column(nullable = false, columnDefinition="TEXT")
     private String body;
 
     @Column(unique = true)
