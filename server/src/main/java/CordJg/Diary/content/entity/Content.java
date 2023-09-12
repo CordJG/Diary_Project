d567c837369397f9965615caebaf137c38c7c6e9
CordJG <wjrmffldglem@gmail.com>
2023-09-12 14:56:12 +0900

    Fix : content body TEXT 타입으로 변경

--- server/src/main/java/CordJg/Diary/content/entity/Content.java
+++ server/src/main/java/CordJg/Diary/content/entity/Content.java
@@ -26,7 +26,7 @@ public class Content  {
     @Column(nullable = false)
     private String title;
 
-    @Column(nullable = false)
+    @Column(columnDefinition = "TEXT")
     private String body;
 
     @Column(unique = true)
